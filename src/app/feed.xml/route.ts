f2069419942397fbf5b8333e2cae7c493ac853ca
Gedroke <ibrahim.elmhadri@gmail.com>
2023-11-26 18:41:04 +0100

    build: trying to solve the navigation issue with updating nextJS

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -10,8 +10,8 @@ export async function GET(req: Request) {
   }
 
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Ibrahim El Mhadri',
+    email: 'ibrahim.procopy@gmail.com',
   }
 
   let feed = new Feed({
